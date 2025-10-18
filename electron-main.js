3ddf8eaa4888491c70b501a07ad9503755605866
dollserver <dollserver@email.com>
2025-10-18 02:10:20 -0300

    Remembers window size

--- electron-main.js
+++ electron-main.js
@@ -1,10 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const windowStateKeeper = require('electron-window-state');
 
 function createWindow() {
+  // Load previous window state (size, position) or use defaults
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 293,
+    defaultHeight: 688
+  });
+
   const win = new BrowserWindow({
-    width: 293,
-    height: 688,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
@@ -12,6 +21,9 @@ function createWindow() {
     autoHideMenuBar: true, // Hides the menu bar
     icon: path.join(__dirname, 'img/icon.png') // Optional: add an icon
   });
+
+  // Let electron-window-state manage and track window size/position changes
+  mainWindowState.manage(win);
 
   win.loadFile('index.html');
 
